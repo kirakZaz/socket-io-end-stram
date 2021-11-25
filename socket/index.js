85b9ae8f849227d07c317a8f2c8b699fa174eb9a
kirakZaz <kirka.zaz@gmail.com>
2021-11-25 21:11:12 +0200

    socket

--- socket/index.js
+++ socket/index.js
@@ -6,28 +6,15 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-const users = [];
-io.on("connection", function (socket) {
-  console.log("A user connected");
-  socket.on("setUsername", function (data) {
-    if (users.indexOf(data) === -1) {
-      users.push(data);
-      socket.emit("userSet", { username: data });
-    } else {
-      socket.emit(
-        "userExists",
-        data + " username is taken! Try some other username."
-      );
-    }
-  });
+io.sockets.on("connection", function (socket) {
+  const buf = Buffer.alloc(454346);
 
-  socket.on("msg", function (data) {
-    //Send message to everyone
-    io.sockets.emit("newmsg", data);
+  console.log(buf);
+  socket.on("ping", function () {
+    socket.emit("pong", buf);
   });
 });
 
-console.log("users", users);
 http.listen(3000, function () {
   console.log("listening on localhost:3000");
 });
